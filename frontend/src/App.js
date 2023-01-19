48ec261a1ad32b894314a24dccfaeb7dae1ead21
Sarah Delmas <54718095+Ikescd@users.noreply.github.com>
2023-01-19 10:19:01 +0100

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 function App() {
-	return <div className='App'>Bienvenue sur Recycle-RAT. Le paRATdis des écolos pauvres.</div>;
+	return (
+		<div className='App'>
+			Bienvenue sur Recycle-RAT. Le paRATdis des écolos à petit budget.
+			<img src='https://cdn.pixabay.com/photo/2018/03/30/06/05/chair-3274474_960_720.jpg' />
+		</div>
+	);
 }
 
 export default App;
