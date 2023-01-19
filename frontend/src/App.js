47278a24291aa9c581d3c251fbaa386c4cff7ad7
Sarah Delmas <54718095+Ikescd@users.noreply.github.com>
2023-01-19 13:59:14 +0100

    add routage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,47 @@
+import React from 'react';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+
 function App() {
 	return (
-		<div className='App'>
-			Bienvenue sur Recycle-RAT. Le paRATdis des écolos à petit budget.
-			<img src='https://cdn.pixabay.com/photo/2018/03/30/06/05/chair-3274474_960_720.jpg' />
-		</div>
+		<Router>
+			<Layout>
+				<Routes>
+					<Route path='/' element={<Homepage />} />
+
+					<Route path='/login' element={<UnderConstruction />} />
+					<Route path='/signup' element={<UnderConstruction />} />
+					<Route path='/passwordReset' element={<UnderConstruction />} />
+
+					<Route path='/profile' element={<UnderConstruction />} />
+
+					<Route path='/products' element={<UnderConstruction />} />
+					<Route path='/products/:id' element={<UnderConstruction />} />
+
+					<Route path='/cart' element={<UnderConstruction />} />
+					<Route path='/orders' element={<UnderConstruction />} />
+					<Route path='/orders/:id' element={<UnderConstruction />} />
+
+					<Route path='/404' element={<NotFound />} />
+					<Route path='*' element={<Navigate to='/404' replace />} />
+				</Routes>
+			</Layout>
+		</Router>
 	);
 }
 
+const Layout = (props) => {
+	return <div>{props.children}</div>;
+};
+
+const UnderConstruction = () => {
+	return <>En construction</>;
+};
+
+const NotFound = () => {
+	return <>Page not found</>;
+};
+
+const Homepage = () => {
+	return <>Bienvenue sur le site de Recycle-RAT</>;
+};
 export default App;
