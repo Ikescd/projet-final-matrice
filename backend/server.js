deb3f4474720a459a1bccfc04f80108c72fc274c
Sarah Delmas <54718095+Ikescd@users.noreply.github.com>
2023-01-20 11:04:17 +0100

    backend/server.js : add get/users route for example (it works)

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
-require('dotenv').config();
+
 const cors = require('cors');
 
 const corsOptions = {
@@ -31,55 +33,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
-	console.log('les users');
-	res.send('les users');
+	connection.query('SELECT * FROM users', (err, result) => {
+		if (err) return res.sendStatus(400);
+		return res.send(result);
+	});
 });
+
 app.listen(3000, () => {
 	console.log('bien connecté');
 });
-
-// const express = require('express');
-// const app = express();
-// const mysql = require('mysql');
-// const bodyParser = require('body-parser');
-// require("dotenv").config();
-// const cors = require("cors");
-// const usersRoutes = require('./routes/users_routes');
-
-// const corsOptions = {
-//     origin: 'http://localhost:5173/'
-// }
-
-// app.use(cors(corsOptions));
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json());
-
-// const port = process.env.PORT || 5000;
-
-// const connectionOptions = {
-//     host : 'localhost',
-//     database : 'Vagabond',
-//     user : 'root',
-//     password : 'root',
-//     port : 8889
-// }
-
-// const connection = mysql.createConnection(connectionOptions)
-//         connection.connect(
-//             function(error){
-//                 if(error)
-//                 {
-//                     throw error;
-//                 }
-//                 else
-//                 {
-//                     console.log('MySQL Database is connected Successfully');
-//                 }
-//             }
-//         )
-//         usersRoutes.getAllUsers(app);
-//         usersRoutes.createUser(app, connection)
-
-// app.listen(5000, ()=> {
-//     console.log(Serveur connecté sur le port : ${port});
-// });
