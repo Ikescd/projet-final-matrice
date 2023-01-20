ba236f79788aacdfa323b585ab7916fc89a79fa7
fvilmot <fvilmot@gmail.com>
2023-01-20 10:15:15 +0100

    mise en page du composant ProductDetails

--- frontend/src/Views/Products/ProductDetails.jsx
+++ frontend/src/Views/Products/ProductDetails.jsx
@@ -15,63 +15,79 @@ export default function ProductDetails() {
 
   return (
     <>
-      <Link
-        component={RouterLink}
-        to={"/"}
-        underline="none"
-        color="text.primary"
-        sx={{ margin: 2 }}
+      <Box
+        component="form"
+        sx={{
+          my: 3,
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          alignItems: "flex-start",
+        }}
       >
-        <Grid container direction="row" alignItems="center">
-          <Grid>
-            <ArrowCircleLeftIcon />
-          </Grid>
-          <Grid>
-            <Typography>Retour à l'accueil</Typography>
-          </Grid>
-        </Grid>
-      </Link>
-      <Box component="form" sx={{ mx: 2 }}>
-        <img
-          src={product.picture}
-          alt={product.name}
-          style={{
-            height: "auto",
-            width: "300px",
-          }}
-        />
-        <Typography variant="h6" sx={{ my: 1 }}>
-          {product.name}
-        </Typography>
-        <Typography variant="h6" sx={{ my: 1 }}>
-          {(product.price / 100).toFixed(2)} €
-        </Typography>
+        <Box sx={{ mx: 2 }}>
+          <Link
+            component={RouterLink}
+            to={"/"}
+            underline="none"
+            color="text.primary"
+          >
+            <Grid container direction="row" alignItems="center">
+              <Grid sx={{ mr: 2 }}>
+                <ArrowCircleLeftIcon />
+              </Grid>
+              <Grid>
+                <Typography>Retour à l'accueil</Typography>
+              </Grid>
+            </Grid>
+          </Link>
 
-        <Typography sx={{ minWidth: 300, maxWidth: 800 }}>
-          {product.description}
-        </Typography>
-        <TextField
-          id="quantity-number"
-          label="Quantité"
-          type="number"
-          size="small"
-          defaultValue="1"
-          inputProps={{ min: 1, max: product.quantityInStock }}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          sx={{ my: 1 }}
-        />
-        <br />
-        <Button
-          variant="contained"
-          sx={{ my: 1 }}
-          onClick={() => {
-            alert("Voir mon panier/ Finaliser mes achats");
-          }}
-        >
-          Ajouter au panier
-        </Button>
+          <img
+            src={product.picture}
+            alt={product.name}
+            style={{
+              height: "auto",
+              width: "340px",
+              marginRight: "5px",
+            }}
+          />
+        </Box>
+
+        <Box sx={{ mx: 2 }}>
+          <Typography variant="h6" sx={{ my: 1, pt: 3 }}>
+            {product.name}
+          </Typography>
+          <Typography variant="h6" sx={{ my: 1 }}>
+            {(product.price / 100).toFixed(2)} €
+          </Typography>
+
+          <Typography sx={{ minWidth: 300, maxWidth: 500 }}>
+            {product.description}
+          </Typography>
+          <TextField
+            id="quantity-number"
+            label="Quantité"
+            type="number"
+            size="small"
+            defaultValue="1"
+            inputProps={{ min: 1, max: product.quantityInStock }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            sx={{ my: 1 }}
+          />
+
+          <Button
+            variant="contained"
+            sx={{ my: 1, mx: 3 }}
+            onClick={() => {
+              alert("Voir mon panier/ Finaliser mes achats");
+            }}
+          >
+            Ajouter au panier
+          </Button>
+        </Box>
       </Box>
     </>
   );
