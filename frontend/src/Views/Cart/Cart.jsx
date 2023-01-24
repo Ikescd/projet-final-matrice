182a937bfa2374d54cc0ac564e3374042ef1b2ac
Sarah Delmas <54718095+Ikescd@users.noreply.github.com>
2023-01-24 15:12:37 +0100

    cut cart.jsx in multiple components

--- frontend/src/Views/Cart/Cart.jsx
+++ frontend/src/Views/Cart/Cart.jsx
@@ -1,75 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import { Box, Link, Typography } from '@mui/material';
+
+import CartList from './CartList';
+
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
 function Cart() {
 	const [cart, setCart] = useState(() => {
 		const localCart = localStorage.getItem('cart');
 		return localCart ? JSON.parse(localCart) : [];
 	});
-	const [productDetails, setProductDetails] = useState([]);
-	let fetchedProduct = new Set();
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		localStorage.setItem('cart', JSON.stringify(cart));
-		const fetchProducts = async () => {
-			for (const product of cart) {
-				if (!fetchedProduct.has(product.productID)) {
-					fetchedProduct.add(product.productID);
-					const response = await fetch(`http://localhost:3000/api/products/${product.productID}`);
-					const data = await response.json();
-					setProductDetails((prevProducts) => [...prevProducts, data]);
-				}
-			}
-		};
-		fetchProducts();
+		setIsLoading(false);
 	}, [cart]);
 
-	const updateProduct = (product, updatedQuantity) => {
-		setCart((prevCart) =>
-			prevCart.map((prod) => {
-				console.log(prod);
-				if (prod.productID === product) {
-					return { ...prod, quantity: updatedQuantity };
-				}
-				return prod;
-			})
-		);
-	};
-
-	const removeProduct = (product) => {
-		setProductDetails((prevProducts) => prevProducts.filter((prod) => prod.id !== product));
-		setCart(cart.filter((prod) => prod.product !== product));
-	};
-
 	return (
-		<div>
-			<h2>Panier</h2>
-			{cart.map((el) => {
-				console.log(cart);
-				let productInCart = {};
+		<Box sx={{ width: '75%', margin: '50px auto' }}>
+			<Link href='/' sx={{ display: 'flex' }}>
+				<KeyboardArrowLeftIcon />
+				<Typography>Retour à l'accueil</Typography>
+			</Link>
 
-				productDetails.map((productEL) => {
-					if (productEL[0].id === el.productID) productInCart = productEL[0];
-				});
+			<Typography variant='h5' sx={{ textAlign: 'center', margin: '50px' }}>
+				Mon panier
+			</Typography>
 
-				const total = el.quantity * productInCart.price;
-
-				return (
-					<div style={{ display: 'flex', justifyContent: 'space-between' }}>
-						<img src={productInCart.picture} alt={productInCart.name} style={{ width: 150 }} />
-						<div>
-							<p>{productInCart.name}</p>
-							<p>{(productInCart.price / 100).toFixed(2)} €</p>
-						</div>
-						<div style={{ display: 'flex' }}>
-							<button onClick={() => updateProduct(productInCart.id, el.quantity + 1)}>+</button>
-							<p>{el.quantity}</p>
-							<button onClick={() => updateProduct(productInCart.id, el.quantity - 1)}>-</button>
-						</div>
-						<div>{(total / 100).toFixed(2)} €</div>
-					</div>
-				);
-			})}
-		</div>
+			{isLoading ? <Typography>Chargement de votre panier...</Typography> : <CartList />}
+			{!isLoading && cart.length == 0 && (
+				<Typography>Il n'y a rien dans votre panier. Voulez-vous faire quelques achats ?</Typography>
+			)}
+		</Box>
 	);
 }
 
