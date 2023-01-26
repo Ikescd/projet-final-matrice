ca5adf8fd3af268938f4d797049728eeb4bec272
FlorianV <fvilmot@gmail.com>
2023-01-26 11:33:31 +0100

    création du fichier indexesRoutes et refactorisation des routes catégories et produits

--- backend/routes/products/productsRoutes.js
+++ backend/routes/products/productsRoutes.js
@@ -1,15 +1,15 @@
-function productsRoutes(app, db) {
-  // get all products
+function getAllProducts(app, db) {
   app.get("/api/products", async (req, res) => {
     await db.query(`SELECT * FROM products`, (err, result) => {
       if (err) throw err;
       res.status(200).send(result);
     });
   });
+}
 
-	// get one product
-	app.get('/api/products/:id', async (req, res) => {
-		const id = req.params.id;
+function getOneProduct(app, db) {
+  app.get("/api/products/:id", async (req, res) => {
+    const id = req.params.id;
 
     await db.query(
       `SELECT * FROM products WHERE id = ?`,
@@ -20,23 +20,24 @@ function productsRoutes(app, db) {
       }
     );
   });
+}
 
-	//Create product
-	app.post('/api/products', async (req, res) => {
-		if (
-			req.body.name &&
-			req.body.description &&
-			req.body.price &&
-			req.body.quantityInStock &&
-			req.body.picture
-		) {
-			const name = req.body.name;
-			const description = req.body.description;
-			const price = req.body.price;
-			const quantityInStock = req.body.quantityInStock;
-			const picture = req.body.picture;
-			const item_code = req.body.item_code;
-			const category_id = req.body.category;
+function createProduct(app, db) {
+  app.post("/api/products", async (req, res) => {
+    if (
+      req.body.name &&
+      req.body.description &&
+      req.body.price &&
+      req.body.quantityInStock &&
+      req.body.picture
+    ) {
+      const name = req.body.name;
+      const description = req.body.description;
+      const price = req.body.price;
+      const quantityInStock = req.body.quantityInStock;
+      const picture = req.body.picture;
+      const item_code = req.body.item_code;
+      const category_id = req.body.category;
 
       const responseDB = await db.query(
         "INSERT INTO products (name, description, price, quantityInStock, picture, item_code, category_id) VALUES (?,?,?,?,?,?,?)",
@@ -58,9 +59,10 @@ function productsRoutes(app, db) {
       res.sendStatus(422);
     }
   });
+}
 
-  //TODO : creation des routes pour supprimer ou modifier un produit
-  /*
+//TODO : creation des routes pour supprimer ou modifier un produit
+/*
   // Update product
   app.put("/api/products/:id", async (req, res) => {});
 
@@ -71,6 +73,5 @@ function productsRoutes(app, db) {
     res.status(200).send(result);
   });
   */
-}
 
-module.exports = productsRoutes;
+module.exports = { getAllProducts, getOneProduct, createProduct };
