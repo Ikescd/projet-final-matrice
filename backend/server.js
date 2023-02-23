b9104a5d5f4edf043cb7ad85be1f1aeb4161bdb3
dsarah <s.delmas.workmail@gmail.com>
2023-02-23 10:09:25 +0100

    add a CORRECT test environnement

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,34 @@
 require('dotenv').config();
-const app = require('./app')
-const indexesRoutes = require('./routes/indexesRoutes')
 const mysql = require('mysql');
+const cors = require('cors');
+
+const { app } = require('./app')
+const indexesRoutes = require('./routes/indexesRoutes')
+
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
 const connectionOptions = {
   host: 'localhost',
   database: 'recycle_rat',
   user: 'root',
   password: process.env.DB_PASSWORD,
-  // port: process.env.DB_PORT
+  // cristiano, il me semble que tu as besoin du port de ton côté ! donc décommente ^^ 
+  //port: process.env.DB_PORT
 };
+
+app.use(cors(corsOptions));
 
 const connection = mysql.createConnection(connectionOptions);
 connection.connect();
+
 indexesRoutes(app, connection)
 
-app.listen(port, () => {
+app.listen(3000, () => {
+  console.log("On the port: " + port)
 })
 
 
